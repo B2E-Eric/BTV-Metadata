323c41857ddb71c50cf0976ebfc990a001ed8a36
clement <clement.saunier@epitech.eu>
2022-05-09 12:02:24 +0200

    Add route for every asset type/numbering

--- src/components/pages/Museum.jsx
+++ src/components/pages/Museum.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import styles from "./Museum.module.css";
 
 export default function Museum() {
-    return (
-        <>
-        MUSEUM PAGE
-        </>
-    )
+  const { type, numbering } = useParams();
+
+  return (
+    <div className={styles.frame}>
+      <div className={styles.card}>
+        Asset type {type ?? "invalid"} n° {numbering ?? "invalid"}
+      </div>
+    </div>
+  );
 }
