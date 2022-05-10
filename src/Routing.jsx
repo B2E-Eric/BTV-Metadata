8c47d11247c7a612eb45dffc41f025e4e96113fe
clement <clement.saunier@epitech.eu>
2022-05-10 16:57:32 +0200

    Museum/ indexing to verify metadatat

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,5 +1,6 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Museum from "./components/pages/Museum";
+import Default from './components/pages/Default';
 
 export default function Routing() {
   return (
@@ -8,6 +9,7 @@ export default function Routing() {
         <Route path="/museum">
           <Route path=":type" element={<Museum/>} />
           <Route path=":type/:numbering" element={<Museum/>} />
+          <Route index element={<Default/>} />
         </Route>
       </Routes>
     </Router>
--- src/components/pages/Default.jsx
+++ src/components/pages/Default.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import styles from "./Default.module.css";
+import { assets } from "../../assets/assets-mainnet";
 
 export default function Default() {
-  return <>DEFAULT PAGE</>;
+  return (
+    <div className={styles.main}>
+      <h1>BTV Museum</h1>
+      <p>
+        This static website is hosted on IPFS and indexes all the Beyond The Void Museum (NFT Collection) metadata.
+      </p>
+
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Rarity</th>
+              <th>Original ERC-20</th>
+            </tr>
+          </thead>
+          <tbody>
+            {[...Array(49)].map((_, idx) => {
+              const asset = assets.find(({type: t}) => t === idx);
+
+              return (
+                <tr key={idx}>
+                  <td className={styles.id}>{idx}</td>
+                  <td>
+                    <img alt="" src={"/museum/" + idx + ".png"} />
+                  </td>
+                  <td className="selectable">{asset?.name}</td>
+                  <td >{asset?.frame}</td>
+                  <td >{asset?.address}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
